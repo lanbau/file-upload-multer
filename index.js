c95b95ae3fee02d6ba2a3be3969e618ee50930dd
lanbau <irvinln@gmail.com>
2015-11-26 22:11:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -10,21 +10,8 @@ app.get('/index.htm', function (req, res) {
 })
 
 app.post('/fileupload', multer({ dest: './uploads/'}).single('file'), function (req, res, next) {
-  console.log(req.body); //form fields
-	/* example output:
-	{ title: 'abc' }
-	 */
+  	console.log(req.body); //form fields
 	console.log(req.file); //form files
-	/* example output:
-            { fieldname: 'upl',
-              originalname: 'grumpy.png',
-              encoding: '7bit',
-              mimetype: 'image/png',
-              destination: './uploads/',
-              filename: '436ec561793aa4dc475a88e84776b1b9',
-              path: 'uploads/436ec561793aa4dc475a88e84776b1b9',
-              size: 277056 }
-	 */
 	res.status(204).end();
 })
 
